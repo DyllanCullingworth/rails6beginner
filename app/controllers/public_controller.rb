b884df6569e7d6d4c66109799dfeb5c982c2be00
dandelion <mmdyllan@gmail.com>
2020-08-05 16:36:58 +0200

    Add Navigation

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,5 +1,13 @@
+# frozen_string_literal: true
+
 class PublicController < ApplicationController
   def homepage
     @posts = Post.all
   end
+
+  def about; end
+
+  def blog; end
+
+  def contact; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
 
   root to: 'public#homepage'
   resources :posts
-
+  get "about" => "public#about", as: :about
+  get "blog" => "public#blog", as: :blog
+  get "contact" => "public#contact", as: :contact
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
