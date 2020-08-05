3ba6f33cdca708794e56b104449b06103a674f86
dandelion <mmdyllan@gmail.com>
2020-08-05 14:58:46 +0200

    scaffold, generate, logger.debug

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
   root to: 'public#homepage'
+  resources :posts
+  get 'welcome/home'
+  get 'welcome/about'
+  get 'welcome/contact'
+  get 'welcome/features'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
